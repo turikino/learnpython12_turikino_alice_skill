de17efeed4d590f994584ba086ab9ccea1cc2c0d
turikino <turikino84@gmail.com>
2019-04-01 22:54:32 +0300

    Сделал скрипт по переводу стихотворения из текстового файла в словарь

--- text2list.py
+++ text2list.py
@@ -10,25 +10,27 @@ import argparse
 def create_parser():
     parser = argparse.ArgumentParser(description='Переводит стихотворение в list по строчкам.')
     parser.add_argument('-f','--file', help='Файл с текстом')
+    parser.add_argument('-id', '--id', help='Номер стихотворения')
     return parser
 
-def text2list(filename):
+def text2list(filename, id):
     with open(filename, mode='r') as f:
         name = os.path.basename(filename).split('.')[0]
         work_list = [line.strip('\n') for line in f.readlines() if line != '\n']
         author = work_list[:1]
         text = work_list[1:]
         poem = {
+            "poem_id": "poem_{}".format(id),
             "author": author,
             "name": name,
             "text": text
         }
-        print(poem)
-    return json.dumps(poem, ensure_ascii=False, indent=2)
+    return poem
 
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
     filename = args.file
-    text2list(filename)
+    id = args.id
+    print(text2list(filename, id))
